015ce41ec9aa2df380bd111e6137a0a972ead646
umgbhalla <umg.bhalla88@gmail.com>
2023-01-13 00:51:19 +0530

    feat(base): updated site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2a1e5947609b44c3bc799a5e46f0b6e8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
