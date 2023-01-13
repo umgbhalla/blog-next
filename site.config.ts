344d9651f192ca18e43f34e5fb6eefc3374b44e6
umgbhalla <umg.bhalla88@gmail.com>
2023-01-13 20:16:02 +0530

    feat(base): misc metadata updates

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Umang\'s Blog',
+  domain: 'blog.umgbhalla.xyz',
+  author: 'Umang Bhalla',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'A list of blogs on Ml/Dev/Ops',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'umgbhalla',
+  github: 'umgbhalla',
+  linkedin: 'umgbhalla',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
